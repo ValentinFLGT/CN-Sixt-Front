e163e0dea6303cccb4883445723cb370b39704b0
ValentinFLGT <valentin.folliguet@le-campu-numerique.fr>
2021-01-27 15:25:12 +0100

    Initial commit

---
+++ src/main/java/com/sixt/sixtfront/controller/VehicleController.java
@@ -0,0 +1,93 @@
+package com.sixt.sixtfront.controller;
+
+import java.util.List;
+
+import com.sixt.sixtfront.model.Vehicle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+class MainController {
+
+    @Autowired
+    private static final RestTemplate restTemplate = new RestTemplate();
+
+    @Value("${welcome.message}")
+    private String message;
+
+    @Value("${error.message}")
+    private String errorMessage;
+
+    @RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
+    public String index(Model model) {
+
+        model.addAttribute("message", message);
+
+        return "index";
+    }
+
+//    @RequestMapping(value = {"/vehicleList"}, method = RequestMethod.GET)
+//    public List vehicleList() {
+//
+//        return restTemplate.getForObject(
+//                "http://sixt-vehicle/vehicle", List.class);
+//    }
+
+    @RequestMapping(value = {"/vehicleList"}, method = RequestMethod.GET)
+    public List<Vehicle> vehicleList(Model model) {
+        String url = "http://sixt-vehicle/vehicle";
+        List<Vehicle> getVehicleList = restTemplate.getForObject(url, List.class);
+        model.addAttribute("vehicleList", getVehicleList);
+
+        return getVehicleList;
+    }
+
+//    @RequestMapping(value = {"/character/{id}"}, method = RequestMethod.GET)
+//    public Character characterById() {
+//
+//        return restTemplate.getForObject(
+//                "http://localhost:8081/character/{id}", Character.class);
+//    }
+//
+//    @RequestMapping(value = {"/addCharacter"}, method = RequestMethod.GET)
+//    public String showAddCharacter(Model model) {
+//
+//        CharacterForm characterForm = new CharacterForm();
+//        model.addAttribute("characterForm", characterForm);
+//
+//        return "addCharacter";
+//    }
+//
+//    @RequestMapping(value = {"/addCharacter"}, method = RequestMethod.POST)
+//    public String saveCharacter(Model model, //
+//                                @ModelAttribute("characterForm") CharacterForm characterForm) throws JSONException {
+//
+//        String name = characterForm.getName();
+//        String gender = characterForm.getGender();
+//
+//        String url = "http://valentin-back/character";
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(MediaType.APPLICATION_JSON);
+//        JSONObject characterJsonObject = new JSONObject();
+//        characterJsonObject.put("name", name);
+//        characterJsonObject.put("gender", gender);
+//        HttpEntity<String> request = new HttpEntity<>(characterJsonObject.toString(), headers);
+//
+//        if (name != null && name.length() > 0 //
+//                && gender != null && gender.length() > 0) {
+//
+//            restTemplate.postForObject(url, request, Character.class);
+//
+//            return "redirect:/characterList";
+//        }
+//
+//        model.addAttribute("errorMessage", errorMessage);
+//        return "addCharacter";
+//    }
+
+}
---
+++ src/main/java/com/sixt/sixtfront/form/VehicleForm.java
@@ -0,0 +1,96 @@
+package com.sixt.sixtfront.form;
+
+public class VehicleForm {
+
+    private Integer id;
+    private String licensePlate;
+    private String type;
+    private String brand;
+    private String model;
+    private String color;
+    private Float rentPrice;
+    private Float kilometerPrice;
+    private Integer horsePower;
+    private Boolean isAvailable;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Float getRentPrice() {
+        return rentPrice;
+    }
+
+    public void setRentPrice(Float rentPrice) {
+        this.rentPrice = rentPrice;
+    }
+
+    public Float getKilometerPrice() {
+        return kilometerPrice;
+    }
+
+    public void setKilometerPrice(Float kilometerPrice) {
+        this.kilometerPrice = kilometerPrice;
+    }
+
+    public Integer getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(Integer horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public Boolean getAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(Boolean available) {
+        isAvailable = available;
+    }
+
+}
